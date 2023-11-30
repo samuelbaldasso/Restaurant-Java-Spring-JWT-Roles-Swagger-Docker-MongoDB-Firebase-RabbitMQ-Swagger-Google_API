dc4333f9aec8a024e5ae2ce51fbbd87d5ba1d3ff
Samuel Baldasso <samuelbaldasso93@gmail.com>
2023-11-30 02:08:06 -0300

    first commit

---
+++ src/main/java/com/sbaldass/combo/ComboApplication.java
@@ -0,0 +1,13 @@
+package com.sbaldass.combo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbaldass/combo/ComboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbaldass.combo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
