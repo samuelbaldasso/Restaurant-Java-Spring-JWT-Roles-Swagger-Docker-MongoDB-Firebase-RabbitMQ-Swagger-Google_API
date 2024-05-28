ce4b5ffe8425fbb3cae489d8e549ee73063b4ba2
samuelbaldasso <samuelbaldasso93@gmail.com>
2024-05-28 12:47:31 -0300

    feat: restaurant entity

--- src/main/java/com/sbaldass/combo/repositories/RoleRepository.java
+++ src/main/java/com/sbaldass/combo/repositories/RoleRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface RoleRepository extends MongoRepository<Role, Long> {
+public interface RoleRepository extends MongoRepository<Role, String> {
     Optional<Role> findByName(RoleName name);
 }
